fd930f5cc03ff25118b2bf93b194277e460818dd
Alexander <doykov70@gmail.com>
2017-10-22 21:56:32 +0300

    week 2 homework task 7 fix

--- 02.recursive-and-iterative-processes/07-ends-with.rkt
+++ 02.recursive-and-iterative-processes/07-ends-with.rkt
@@ -6,7 +6,7 @@
 
 (define (ends-with? number test)
   (cond
-    ((= test 0) #t)
+    ((< test 10) (if (eq? (modulo number 10) (modulo test 10)) #t #f))
     ((not (eq? (modulo number 10) (modulo test 10))) #f)
     (else (ends-with? (quotient number 10) (quotient test 10)))
   )
@@ -17,6 +17,7 @@
     (check-true (ends-with? 8317 17))
     (check-true (ends-with? 82 82))
     (check-false (ends-with? 8213 31))
+    (check-false (ends-with? 211 0))
     (check-true (ends-with? 210 0))
     (check-false (ends-with? 2921 2))
   )
